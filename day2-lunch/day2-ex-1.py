2f03dd6114152807023e7ffb2ea75d7d9eede29b
xiuqiqi <nxiuqichen@gmail.com>
2016-08-30 14:47:52 -0400

    Correct Basic 1

--- day2-lunch/day2-ex-1.py
+++ day2-lunch/day2-ex-1.py
@@ -7,6 +7,9 @@ n=0
 for line in sys.stdin:
     if line.startswith("@"):
         continue
+    fields=line.rstrip("\r\n").split("\t")
+    if fields[2] == "*":
+        continue
     n=n+1
     
 print n
