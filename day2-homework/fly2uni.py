c25ca53115f36fc53333c16f3ed382e1e6a8c9c3
xiuqiqi <nxiuqichen@gmail.com>
2016-08-30 22:55:51 -0400

    Correct Q1, string

--- day2-homework/fly2uni.py
+++ day2-homework/fly2uni.py
@@ -7,6 +7,6 @@ for line in sys.stdin:
     if "DROME" not in line:
         continue
         # select only the lines with ID
-    print line[52:63], "\t", line[40:46]
-    # first print the Flybase ID, a <TAB>, then UniProt ID
+    print "".join(line[52:63]), "\t", "".join(line[40:46])
+    # first print the Flybase ID, a <TAB>, then UniProt ID, in string
     
