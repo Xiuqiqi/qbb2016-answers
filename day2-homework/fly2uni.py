3fe11c0a1ff42929861c98474f860b631c5aeef4
xiuqiqi <nxiuqichen@gmail.com>
2016-08-31 00:36:50 -0400

    correct Q1 space

--- day2-homework/fly2uni.py
+++ day2-homework/fly2uni.py
@@ -7,6 +7,6 @@ for line in sys.stdin:
     if "DROME" not in line:
         continue
         # select only the lines with ID
-    print line[52:62], "\t", line[40:46]
+    print "%s\t%s" % (line[52:63], line[40:46])
     # first print the Flybase ID, a <TAB>, then UniProt ID, in string
     
